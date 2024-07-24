e69bcb9670234ca7e68a823f7b727a58f06f760c
Ryan Chen <ryanchen979@gmail.com>
2024-07-23 20:54:39 -0400

    kms

---
+++ dreamyuni/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Head>
+      <Title>Dreamy Uni</Title>
+      <Meta name="description" content="Dream the Cream" />
+    </Head>
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  script: [{ innerHTML: "console.log('Hello world')" }],
+});
+</script>
